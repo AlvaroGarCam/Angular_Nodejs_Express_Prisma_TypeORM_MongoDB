c60ea0ddaea1f94fc4aa462306a4d329aabe0d02
AlvaroGarCam <alvgarcam@alu.edu.gva.es>
2024-10-28 14:51:01 +0100

    Refactor companyGetById controller to use email extracted from token instead of id

--- backend_prisma/src/routes/api/company.routes.ts
+++ backend_prisma/src/routes/api/company.routes.ts
@@ -6,20 +6,48 @@ import validatorCreate from '../../middleware/companyValidators/companyCreateVal
 import { roleMiddleware } from '../../middleware/companyValidators/RoleMiddleware';
 import companyLogin from "../../controllers/companyController/companyLogin.controller";
 import validatorLogin from "../../middleware/companyValidators/companyLoginValidator";
+import { updateFollowers } from "../../controllers/companyController/companyFollowers.controller";
+import updateCompany from "../../controllers/companyController/updateCompanty.controller";
+import authMiddleware from "../../middleware/authMiddleware";
 
 const router = Router();
 
 router.get("/company/:id", validatorListOne, (req: Request, res: Response, next: NextFunction) => {
     companyGetById(req, res, next);
 });
-router.post("/register", validatorCreate, (req: Request, res: Response, next: NextFunction) => {
+router.post("/company/register", validatorCreate, (req: Request, res: Response, next: NextFunction) => {
     companyCreate(req, res, next);
 });
-router.get('/dashboard', roleMiddleware('company'), (req, res) => {
+router.get('/company/dashboard', roleMiddleware('company'), (req, res) => {
     res.json({ message: 'Welcome to the company dashboard' });
 });
-router.post('/login', validatorLogin, (req: Request, res: Response, next: NextFunction) => {
+router.post('/company/login', validatorLogin, (req: Request, res: Response, next: NextFunction) => {
     companyLogin(req, res, next);
 });
+router.put('/company/follow/:companyId', (req: Request, res: Response) => {
+    updateFollowers(req, res);
+});
+router.put(
+    "/company",
+    authMiddleware,  // Middleware de autenticación
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await updateCompany(req, res, next);
+        } catch (error) {
+            next(error);  // Manejo de errores
+        }
+    }
+);
+router.get(
+    "/company",
+    authMiddleware,  // Middleware de autenticación
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await companyGetById(req, res, next);
+        } catch (error) {
+            next(error);  // Manejo de errores
+        }
+    }
+);
 
 export default router;
